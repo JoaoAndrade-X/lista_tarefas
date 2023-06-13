c717fdcf4f62be26bd75aed7e58ef4e4019a1b35
OVerdadeiroX <xtudo.vestibulares@gmail.com>
2023-06-13 20:14:28 +0100

    Creating List

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _toDoList = [];
+  List _toDoList = ["Daniel", "Joao"];
 
   @override
   Widget build(BuildContext context) {
@@ -37,24 +37,41 @@ class _HomeState extends State<Home> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: "Nova Tarefa",
-                        labelStyle: TextStyle(
-                          color: Colors.blueAccent,
-                        ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Nova Tarefa",
+                      labelStyle: TextStyle(
+                        color: Colors.blueAccent,
                       ),
                     ),
+                  ),
                 ),
                 RaisedButton(
                   color: Colors.blueAccent,
                   child: Text("ADD"),
                   textColor: Colors.white,
-                  onPressed: (){},
+                  onPressed: () {},
                 ),
               ],
             ),
-          )
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.only(top: 10.0),
+              itemCount: _toDoList.length,
+              itemBuilder: (context, index) {
+                return CheckboxListTile(
+                  title: Text(_toDoList[index]["title"]),
+                  value: _toDoList[index]["ok"],
+                  secondary: CircleAvatar(
+                    child: Icon(
+                      _toDoList[index]["ok"] ? Icons.check : Icons.error
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
