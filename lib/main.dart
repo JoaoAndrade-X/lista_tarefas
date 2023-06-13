84e3b92ac0d95247afe4678e8de3fff1b293caa5
OVerdadeiroX <xtudo.vestibulares@gmail.com>
2023-06-13 21:53:12 +0100

    Creating Snackbar to Undo Action

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
   @override
   void initState() {
@@ -109,17 +111,46 @@ class _HomeState extends State<Home> {
         title: Text(_toDoList[index]["title"]),
         value: _toDoList[index]["ok"],
         secondary: CircleAvatar(
-          child: Icon(
-              _toDoList[index]["ok"] ? Icons.check : Icons.error
-          ),
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
         ),
         onChanged: (c) {
-          setState(() {
-            _toDoList[index]["ok"] = c;
-            _saveData();
-          });
+          setState(
+            () {
+              _toDoList[index]["ok"] = c;
+              _saveData();
+            },
+          );
         },
       ),
+      onDismissed: (direction) {
+        setState(
+          () {
+            _lastRemoved = Map.from(_toDoList[index]);
+            _lastRemovedPos = index;
+            _toDoList.removeAt(index);
+
+            _saveData();
+
+            final snack = SnackBar(
+              content: Text("Tarefa \"${_lastRemoved["tilte"]}\" removida!"),
+              action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(
+                    () {
+                      _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                      _saveData();
+                    },
+                  );
+                },
+              ),
+              duration: Duration(seconds: 3),
+            );
+
+            Scaffold.of(context).showSnackBar(snack);
+          },
+        );
+      },
     );
   }
 
